7efc9cf0163de58392ca0c0fe5fce5a6ddcab841
Christopher van Zomeren <vanzomerenc@gmail.com>
2018-07-22 18:44:50 +0000

    Fix test-data-processing

--- blocks/signal_detection_method/__init__.py
+++ blocks/signal_detection_method/__init__.py
@@ -10,6 +10,12 @@ available_detection_methods = {
 def select_detection_method(name):
     if name not in available_detection_methods:
         print('Unrecognized detection method: {}'.format(name))
+        print_detection_methods()
         sys.exit(-1)
     else:
         return available_detection_methods[name]
+
+def print_detection_methods():
+    print('Available detection methods:')
+    for k in available_detection_methods.keys():
+        print('\t{0}'.format(k))
