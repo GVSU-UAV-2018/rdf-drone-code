c8c6050cadc9edbe1e7991785fa3a9dccf90d3f0
willisoa <willisoa@mail.gvsu.edu>
2018-07-22 23:25:37 -0400

    Removed test folder. In uav-rdf-top-module removed flushes, and pi hb calls

---
+++ test/2015_AS/RDF_Detection_No_GUI.py
@@ -0,0 +1,138 @@
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Rdf Detection No Gui
+# Generated: Mon Oct 26 23:51:08 2015
+##################################################
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import fft
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from optparse import OptionParser
+from radiosource import RadioSource
+from collar_detect import collar_detect
+import sys
+import time
+import time
+import math
+import fcdproplus
+
+fft_size = 512
+samp_rate = 93750
+collar_freq = 150801000
+freq_offset = 3000
+bandwidth = 1000
+gains = [9, 1, 1] #[1, 1, 15] #LNA: 1, MIXER: 1, IF: 20 as per prev group
+SNR = 5.0
+
+class RDF_Detection_No_GUI(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Rdf Detection No Gui")
+
+        ##################################################
+        # Variables
+        ##################################################
+        global fft_size
+        global samp_rate
+        global collar_freq
+        global freq_offset
+        global bandwidth	
+        global gains
+        global SNR
+        self.fft_size = fft_size
+        self.samp_rate = samp_rate
+        self.collar_freq = collar_freq 
+        self.freq_offset = freq_offset
+        self.bandwidth = bandwidth
+        self.gains = gains
+        self.SNR = SNR
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.source = RadioSource(
+            preferred_sample_rate=self.samp_rate,
+            gains=self.gains,
+            frequency_offset=self.freq_offset,
+            signal_frequency=self.collar_freq)
+        #self.fcdproplus_fcdproplus_0 = fcdproplus.fcdproplus("",1)
+        #self.fcdproplus_fcdproplus_0.set_lna(1)
+        #self.fcdproplus_fcdproplus_0.set_mixer_gain(1)
+        #self.fcdproplus_fcdproplus_0.set_if_gain(20) #20
+        #self.fcdproplus_fcdproplus_0.set_freq_corr(0)
+        #self.fcdproplus_fcdproplus_0.set_freq(self.collar_freq - self.freq_offset)
+        
+        self.fft_vxx_0 = fft.fft_vfc(self.fft_size, True, (window.rectangular(self.fft_size)), 1)
+        self.collar_detect_Burst_Detection_0 = collar_detect(self.samp_rate, self.fft_size, 
+            self.freq_offset, self.bandwidth, self.SNR)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, self.fft_size)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(self.fft_size)
+        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(self.fft_size)
+        self.band_pass_filter_0 = filter.fir_filter_ccf(1, firdes.band_pass( #1 instead of 12 to keep 93750 rate
+            100, self.samp_rate, self.freq_offset - (self.bandwidth / 2),
+            self.freq_offset + (self.bandwidth / 2), 600, firdes.WIN_RECTANGULAR, 6.76))
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.source, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
+        self.connect((self.fft_vxx_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.fft_vxx_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.collar_detect_Burst_Detection_0, 0))
+
+    '''def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.band_pass_filter_0.set_taps(firdes.band_pass(100, self.samp_rate, 2.5e3, 3.5e3, 600, firdes.WIN_RECTANGULAR, 6.76))
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.fcdproplus_fcdproplus_0.set_if_gain(self.gain)
+
+    def get_collar_freq(self):
+        return self.collar_freq
+
+    def set_collar_freq(self, freq):
+        global collar_freq
+        collar_freq = freq
+        self.fcdproplus_fcdproplus_0.set_freq(collar_freq - 3000)
+
+    def get_SNR(self):
+        return self.SNR
+
+    def set_SNR(self, snr):
+	global SNR
+        SNR = snr
+	print SNR
+	
+    def update_vars(self, rcvd_msg):
+	global bearing_deg
+	global collar_freq
+	collar_freq = rcvd_msg.data[0]
+	self.collar_detect_Burst_Detection_0.update_SNR(rcvd_msg.data[2])
+	self.collar_detect_Burst_Detection_0.update_scanning(rcvd_msg.scanning,bearing)
+	self.fcdproplus_fcdproplus_0.set_freq(collar_freq - 3000)
+	self.fcdproplus_fcdproplus_0.set_if_gain(rcvd_msg.data[1])'''
+
+sigproc = RDF_Detection_No_GUI()
+
+while True:
+    sigproc.start()
+    time.sleep(5)
+    sigproc.stop()
+    sigproc.wait()
---
+++ test/2015_AS/radiosource.py
@@ -0,0 +1,102 @@
+from fractions import Fraction
+
+from gnuradio import gr
+from gnuradio import blocks
+from gnuradio import filter as gr_filter
+import osmosdr
+
+"""
+Wrapper for osmosdr block.
+Sets all parameters when creating the block, rather than providing separate
+functions to set them.
+Automatically performs filtering and resampling to provide a sample rate independent of the sample rates supported by the receiver.
+"""
+
+class RadioSource(gr.hier_block2):
+
+    def __init__(self, preferred_sample_rate, gains, frequency_offset, signal_frequency):
+        
+        gr.hier_block2.__init__(self, "Radio Source",
+            gr.io_signature(0, 0, gr.sizeof_gr_complex),
+            gr.io_signature(1, 1, gr.sizeof_gr_complex))
+        
+        ##################################################
+        # Blocks
+        ##################################################
+        self.source = osmosdr.source('sensitivity')
+        
+        self.source.set_center_freq(signal_frequency - frequency_offset)
+        
+        self._set_gains(gains)
+        self._set_closest_possible_sample_rate(preferred_sample_rate)
+        self._set_closest_possible_prefilter(preferred_sample_rate)
+        resample_ratio = self._calc_resample_ratio(preferred_sample_rate)
+        self.resample = gr_filter.rational_resampler_ccc(
+            interpolation=resample_ratio.numerator,
+            decimation=resample_ratio.denominator)
+        
+        print('Selected output sample rate', self.sample_rate())
+        
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect(self.source, self.resample, self)
+    
+    def sample_rate(self):
+        return (
+            int(self.source.get_sample_rate())
+            * int(self.resample.resampler.interpolation())
+            / int(self.resample.resampler.decimation()))
+
+    def get_center_frequency(self):
+        return self.source.get_center_freq()
+
+
+    def _set_gains(self, gains):
+        gain_stage_names = self.source.get_gain_names()
+        actual_gains = [
+            self.source.set_gain(g, n) for g, n in zip(gains, gain_stage_names)]
+        if actual_gains == gains:
+            print 'Set all gains'
+        else:
+            print 'Failed to set all gains'
+        print('Gain stages', gain_stage_names)
+        print('Attempted  ', gains)
+        print('Actual     ', actual_gains)
+
+    def _set_closest_possible_sample_rate(self, preferred_sample_rate):
+        try:
+            best_available_sample_rate = min(
+                filter(
+                    lambda r: r >= preferred_sample_rate,
+                    self.source.get_sample_rates().values()))
+            self.source.set_sample_rate(best_available_sample_rate)
+        except ValueError:
+            print 'Could not set internal sample rate'
+        print(
+            'Available sample rates: ',
+            self.source.get_sample_rates().values())
+        print('Selected input sample rate', self.source.get_sample_rate())
+
+    def _set_closest_possible_prefilter(self, preferred_sample_rate):
+        try:
+            best_available_prefilter = min(
+                filter(
+                    lambda r: r >= preferred_sample_rate,
+                    self.source.get_bandwidth_range().values()))
+            self.source.set_bandwidth(best_available_prefilter)
+        except ValueError:
+            print 'Could not set prefilter'
+        print(
+            'Available prefilters: ',
+            self.source.get_bandwidth_range().values())
+        print('Selected prefilter', self.source.get_bandwidth())
+
+    def _calc_resample_ratio(self, preferred_sample_rate):
+        result = Fraction(
+            int(preferred_sample_rate),
+            int(self.source.get_sample_rate()))
+        print('Interpolation', result.numerator)
+        print('Decimation', result.denominator)
+        return result
