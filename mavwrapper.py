0319dc5d3a6a69bdd253bb5f7391428f51664c0c
Christopher van Zomeren <vanzomerenc@gmail.com>
2018-07-29 21:11:13 +0000

    Fix MavParamTable and add send_heartbeat function to mavwrapper
    MavParamTable now should have a callback per parameter instead
    of a single table-wide callback.
    
    Trying to handle `param_id`s not in the table shouldn't crash the program.
    
    The `param_index` in PARAM_REQUEST_READ should be respected now.

--- mavwrapper.py
+++ mavwrapper.py
@@ -1,35 +1,40 @@
-# Pymavlink is... mostly not documented, so most of our knowledge of it comes
-# from reading the (mostly auto-generated) code. This works, but we might be
-# doing some things that aren't intended, because we don't always know _what_
-# was intended.
-#
-# We are using pymavlink.mavutil because it's convenient.
-# However, it is _not_ the proper abstraction.
-# mavutil assumes that it's on the end of the connection which sends commands
-# and receives telemetry, but we're on the end which receives commands and
-# sends telemetry, which is exactly the opposite.
-#
-# We are _not_ using pymavlink.mavparm because, again, it assumes we are on
-# the commanding end of the connection, which we aren't.
-#
-# If we had more time, we might either modify mavutil so that it can work on
-# the 'vehicle' end of a connection without surprises, or add a separate module
-# to do so.
-
 from collections import namedtuple
 
-PARAM_ID_MAX_LENGTH = 16 # from MAVLink common message set specification
-PARAM_TYPE_REAL64 = 10 # from MAVLink common message set specification
+# from MAVLink common message set specification
+PARAM_ID_MAX_LENGTH = 16
+PARAM_TYPE_REAL64 = 10
+MAV_TYPE_ONBOARD_CONTROLLER = 18
+MAV_TYPE_AUTOPILOT_INVALID = 8
+MAV_STATE_ACTIVE = 4
 
-MavParam = namedtuple('MavParam', ['name', 'value'])
+
+def send_heartbeat(connection, status=MAV_STATE_ACTIVE):
+    connection.heartbeat_send(
+        MAV_TYPE_ONBOARD_CONTROLLER,
+        MAV_TYPE_AUTOPILOT_INVALID,
+        0,
+        0,
+        status)
+
+
+MavParam = namedtuple('MavParam', ['name', 'value', 'set_param'])
+
+def _set_param(param):
+    if param.set_param is not None:
+        param.set_param(param)
 
 class MavParamTable(object):
     """
     This class represents parameters stored on this machine (i.e. acting as
     the vehicle end of a MAVLink connection).
-    Parameters are determined when this object is created, by passing
+
+    Parameter names are determined when this object is created, by passing
     a list of `MavParam`s to the constructor. They cannot be changed except
     by replacing the entire `MavParamTable` object with a new one.
+
+    Each parameter may have a `set_param` callback that is called with the
+    parameter's MavParam object every time the parameter is changed.
+    Parameters which don't need a callback can set this to None.
 
     Parameters appear as regular object members, with a few limitations:
 
@@ -39,19 +44,16 @@ class MavParamTable(object):
       which is a 64-bit float.
     """
 
-    __slots__ = '_params', '_lookup', '_handlers', '_on_param_changed'
+    __slots__ = '_params', '_lookup', '_handlers'
 
-    def __init__(self, *params, on_param_changed = None):
+    def __init__(self, *params):
         """
         :param params:
             The list of parameters stored in this object.
-        :param on_param_changed:
-            An (optional) function to call when a parameter is set on this
-            object. on_param_changed receives this object and the name of
-            the changed parameter.
         """
         object.__setattr__(self, '_params',
-            [MavParam(param.name, float(param.value)) for param in params])
+            [MavParam(param.name, float(param.value), param.set_param)
+                for param in params])
         object.__setattr__(self, '_lookup',
             {k: i for k, i in zip(
                 [param.name for param in params],
@@ -61,12 +63,11 @@ class MavParamTable(object):
             'PARAM_REQUEST_LIST': self.__class__._handle_PARAM_REQUEST_LIST,
             'PARAM_SET': self.__class__._handle_PARAM_SET
             })
-        object.__setattr__(self, '_on_param_changed', on_param_changed)
 
 
     def __getattr__(self, k):
         if k in self.__slots__:
-            return object.__getattr__(k)
+            return object.__getattr__(self, k)
         try:
             return self._params[self._lookup[k]].value
         except KeyError:
@@ -75,9 +76,11 @@ class MavParamTable(object):
     def __setattr__(self, k, v):
         if k not in self._lookup:
             raise AttributeError(k)
-        self._values[self.lookup[k]] = MavParam(k, float(v))
-        if self._on_param_changed:
-            self._on_param_changed(self, param.name)
+        i = self._lookup[k]
+        old_value = self._params[i]
+        result = MavParam(k, float(v), old_value.set_param)
+        result.set_param(result)
+        self._params[i] = result
 
 
     def __repr__(self):
@@ -87,31 +90,35 @@ class MavParamTable(object):
     def handle(self, connection, message):
         """
         Handle an incoming message and use connection to send any response.
+        :param connection:
+            The connection the message was received on.
+        :param message:
+            The received message.
         """
         h = self._handlers.get(message.get_type())
         if h is not None:
             h(self, connection, message)
 
-    def _send_param(self, connection, k):
-        i = self._lookup[k]
+    def _send_param(self, connection, k, i):
+        if i < 0:
+            i = self._lookup[k]
         v = self._params[i].value
         connection.param_value_send(
             k, v, PARAM_TYPE_REAL64, len(self._params), i)
         
 
     def _handle_PARAM_REQUEST_READ(self, connection, message):
-        if message.param_id not in self._lookup:
-            return
-        self._send_param(connection, message.param_id)
+        try:
+            self._send_param(connection, message.param_id, message.param_index)
+        except Exception as ex:
+            print('{0} failed: {1}'.format(message.get_type(), ex))
 
     def _handle_PARAM_REQUEST_LIST(self, connection, message):
         for param in self._params:
-            self._send_param(param.name)
+            self._send_param(param.name, -1)
 
     def _handle_PARAM_SET(self, connection, message):
-        if message.param_id not in self._lookup:
-            return
-        self._values[self.lookup[message.param_id]] = MavParam(k, float(v))
-        if self._on_param_changed:
-            self._on_param_changed(self, param.name)
-        self._send_param(param.name)
+        try:
+            self.__setattr__(message.param_id, message.param_value)
+        except Exception as ex:
+            print('{0} failed: {1}'.format(message.get_type(), ex))
